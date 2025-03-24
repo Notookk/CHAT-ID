a65de2b406a19769d808d33aa501e39c7729adda
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-03-24 18:09:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@
 API_ID = 'YOUR_API_ID'          # Get from https://my.telegram.org
 API_HASH = 'YOUR_API_HASH'      # Get from https://my.telegram.org
 PHONE_NUMBER = 'YOUR_PHONE_NUMBER'  # Your phone number with country code
+OWNER_ID = 7875192045
